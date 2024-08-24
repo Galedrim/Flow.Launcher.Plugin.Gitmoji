82d9baf274c6787ab452b6d586bacb526c4ccb99
Galedrim <84284891+Galedrim@users.noreply.github.com>
2024-08-24 12:47:44 +0200

    :recycle: Improve structure with Dataclass and Cattrs

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 # -*- coding: utf-8 -*-
-import sys,os
+import sys
+import os
+
 parent_folder_path = os.path.abspath(os.path.dirname(__file__))
 sys.path.append(parent_folder_path)
 sys.path.append(os.path.join(parent_folder_path, 'lib'))
 sys.path.append(os.path.join(parent_folder_path, 'plugin'))
 
-from plugin.gitmoji import Gitmoji
+from plugin.gitmoji import GitmojiLoader  # noqa: E402
 
 if __name__ == "__main__":
-    Gitmoji()
+    GitmojiLoader()
--- plugin/gitmoji.py
+++ plugin/gitmoji.py
@@ -1,90 +1,106 @@
 import json
-from urllib import request
-
+import cattr
 import os
 import webbrowser
 
-from flox import Flox, ICON_BROWSER, ICON_COPY
+from dataclasses import dataclass
+from typing import List
+
+from flox import Flox
 from flox.clipboard import Clipboard
 
-ICON_FOLDER = "./Icons"
+ICONS_FOLDER= r"./Icons"
+CLIPBOARD_ICON = r"./Images/clipboard_icon.png"
+GITMOJI_JSON_FILE = r"./data/gitmojis.json"
 
-class Gitmoji(Flox, Clipboard):
-   
-    def find_file_with_emoji_code(self, emoji_code):  
-        found_file = None   
-        for root, dirs, files in os.walk(ICON_FOLDER):
+@dataclass
+class Gitmoji:
+    emoji: str = ""
+    entity: str = ""
+    code: str = ""
+    description: str = ""
+    name: str = ""
+    example: str = ""
+
+class GitmojiLoader(Flox, Clipboard):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        with open(GITMOJI_JSON_FILE, "r", encoding="utf-8") as file:
+            data = json.load(file)
+            converter = cattr.Converter()
+            self.gitmojis = [converter.structure(item, Gitmoji) for item in data]
+
+    def results(self, query):
+        for gitmoji in self.gitmojis:
+            comparable_items = self._get_gitmoji_comparable_item(gitmoji)
+            if any(self.match(query, str(item)) for item in comparable_items):
+                self.add_item(
+                    title=self._get_gitmoji_title(gitmoji),
+                    subtitle=self._get_gitmoji_subtitle(gitmoji),
+                    icon=self._get_gitmoji_icon(gitmoji),
+                    context=[gitmoji.emoji],
+                    method=self.copy_to_clipboard,
+                    parameters=[gitmoji.emoji],
+                )
+        return self._results
+
+    def _get_gitmoji_comparable_item(self, gitmoji:Gitmoji) -> List[str]:
+        return [gitmoji.name, gitmoji.description, gitmoji.emoji]
+    
+    def _get_gitmoji_title(self, gitmoji:Gitmoji) -> str:
+        return f"[{gitmoji.name}] {gitmoji.description}"
+    
+    def _get_gitmoji_subtitle(self, gitmoji:Gitmoji) -> str:
+        return f"{gitmoji.example}"
+
+    def _get_gitmoji_icon(self, gitmoji:Gitmoji) -> str:
+        found_file = None
+        for root, dirs, files in os.walk(ICONS_FOLDER):
             for file_name in files:
+                emoji_code = self.emoji_to_code(gitmoji.emoji)
                 if emoji_code in file_name:
-                     found_file = os.path.join(ICON_FOLDER, file_name)
-                     break
+                    found_file = os.path.join(ICONS_FOLDER, file_name)
+                    break
             return found_file
-  
-    def emoji_to_code(self, emoji):
+        
+    def emoji_to_code(self, emoji: str) -> str:
         code = []
         for char in emoji:
-            char_code = '{:X}'.format(ord(char)).lower()
+            char_code = "{:X}".format(ord(char)).lower()
             code.append(char_code)
-        
+
         emoji_code = "-".join(code)
         return emoji_code
-    
-    def match(self, query, name, description):
-        if query == '':
+
+    def match(self, query: str, item: str):
+        if query == "":
             return True
         q = query.lower()
-        
-        if q in name.lower():
+
+        if q in item.lower():
             return True
-        if q in description.lower():
-            return True
-            
-    def results(self, query):
 
-        with request.urlopen("https://gitmoji.dev/api/gitmojis") as response:
-            status_code = response.getcode()
-            if status_code == 200:
-                data = response.read().decode('utf-8') 
-                emoji_data = json.loads(data)
-
-        for gitmoji in emoji_data['gitmojis']:
-            if self.match(query, gitmoji['name'], gitmoji['description']):
-                emoji_code = self.emoji_to_code(gitmoji['emoji']) 
-                icon = self.find_file_with_emoji_code(emoji_code)        
-                self.add_item(
-                    title=gitmoji['name'],
-                    subtitle=gitmoji['description'],
-                    icon=icon,
-                    context=[gitmoji['emoji']],
-                    method = self.copy_emoji,
-                    parameters=[gitmoji['emoji']]
-                ) 
-        
-        return self._results
-    
-    def context_menu(self, data):
-        emoji = data[0]
-
+    def context_menu(self, emoji: str):
         self.add_item(
-            title="Copy",
+            title="Copy Emoji",
             subtitle="Copy emoji to clipboard",
-            icon=ICON_COPY,
-            method=self.copy_emoji,
+            icon=CLIPBOARD_ICON,
+            method=self.copy_to_clipboard,
             parameters=[emoji],
         )
         self.add_item(
-            title='Open Gitmoji.dev',
-            subtitle='Opens Gitmoji.dev',
-            icon=ICON_BROWSER,
+            title="Open Gitmoji.dev",
+            subtitle="Opens Gitmoji.dev",
             method=self.open_url,
-            parameters=[f'https://gitmoji.dev/']
+            parameters=["https://gitmoji.dev/"],
         )
 
-    def open_url(self, url):
+    def open_url(self, url: str):
         webbrowser.open(url)
-    
-    def copy_emoji(self, emoji):
-        self.put(emoji)
-                          
-    def query(self, query):
+
+    def copy_to_clipboard(self, item: str):
+        self.put(item)
+
+    def query(self, query: str):
         self.results(query)
